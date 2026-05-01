d5efefb2e35b76a6c085e0a9819c04b621488728
Rebecca <rebecca_517@hotmail.com>
2026-05-01 12:12:20 -0400

    add menu styling

--- my-react-app/src/components/menu/Menu.tsx
+++ my-react-app/src/components/menu/Menu.tsx
@@ -1,11 +1,13 @@
+import './Menu.css'
+
 export default function Menu() {
     return (
-      <div>
+      <div id="menu">
           <ul>
               <li><a>Home</a></li>
               <li><a href="Portfolio">Portfolio</a></li>
               <li><a href="#About">About</a></li>
-              <li><a>Contact</a></li>
+              <li><a href="Contact">Contact</a></li>
           </ul>
       </div>
     )

e1e8428f8b8c54d41e0055b84d247fbeb5db0d27
Rebecca <rebecca_517@hotmail.com>
2026-05-01 11:49:06 -0400

    set up

---
+++ my-react-app/src/menu/Menu.tsx
@@ -0,0 +1,12 @@
+export default function Menu() {
+    return (
+      <div>
+          <ul>
+              <li><a href="#About">About</a></li>
+              <li><a>Portfolio</a></li>
+              <li><a>Contact</a></li>
+          </ul>
+      </div>
+    )
+  }
+  

681c4b0379492410208e8828bcd8338671b5445e
Rebecca <rebecca_517@hotmail.com>
2026-05-01 12:05:52 -0400

    set up components and pages directories

--- my-react-app/src/App.tsx
+++ my-react-app/src/App.tsx
@@ -1,15 +1,13 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
-import Menu from './menu/Menu'
+import Menu from './components/menu/Menu'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <div>abc</div>
       <Menu></Menu>
     </>
   )
--- my-react-app/src/components/menu/Menu.tsx
+++ my-react-app/src/components/menu/Menu.tsx
@@ -2,8 +2,9 @@ export default function Menu() {
     return (
       <div>
           <ul>
+              <li><a>Home</a></li>
+              <li><a href="Portfolio">Portfolio</a></li>
               <li><a href="#About">About</a></li>
-              <li><a>Portfolio</a></li>
               <li><a>Contact</a></li>
           </ul>
       </div>

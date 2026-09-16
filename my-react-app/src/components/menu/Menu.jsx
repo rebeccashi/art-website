4be5c65f3c5038f157eb57cb0ca3edd12ba63aae
Rebecca <rebecca_517@hotmail.com>
2026-09-16 14:34:53 -0400

    change styling of menu

--- my-react-app/src/components/menu/Menu.jsx
+++ my-react-app/src/components/menu/Menu.jsx
@@ -6,15 +6,15 @@ export default function Menu() {
     return (
       <div id="menu">
           <ul>
-             <Link to='/' className='link'>Home</Link>
-             <Link to={EXTERNAL_URLS.store} className='link' target="_blank" rel="noopener">
+             <Link to='/' className='menu-item'>Home</Link>
+             <Link to={EXTERNAL_URLS.store} className='menu-item' target="_blank" rel="noopener">
                Shop
              </Link>
-             <Link to='/portfolio' className='link'>
+             <Link to='/portfolio' className='menu-item'>
                Portfolio
              </Link>
-             <Link to='/about' className='link'>About</Link>
-             {/* <Link to='/contact' className='link'>Contact</Link> */}
+             <Link to='/about' className='menu-item'>About</Link>
+             {/* <Link to='/contact' className='menu-item'>Contact</Link> */}
           </ul>
       </div>
     )

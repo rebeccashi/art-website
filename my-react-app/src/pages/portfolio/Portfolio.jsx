ca33ef72e89f0205ab720bec505010bcb27ca64b
Rebecca <rebecca_517@hotmail.com>
2026-09-16 14:29:42 -0400

    add menu to portfolio page

--- my-react-app/src/pages/portfolio/Portfolio.jsx
+++ my-react-app/src/pages/portfolio/Portfolio.jsx
@@ -3,38 +3,27 @@ import SeriesList from "@/components/SeriesList/SeriesList";
 import SeriesPage from "@/components/SeriesPage/SeriesPage";
 import seriesList from "@/data/seriesList";
 import "./Portfolio.css";
+import Menu from '../../components/menu/Menu'
 
-/**
- * Portfolio
- *
- * Top-level component. Renders either the SeriesList (all series)
- * or a SeriesPage (single series + works grid) depending on which
- * series the user has selected.
- *
- * Props:
- *   series {Array} - Optional. Array of series objects.
- *                    Defaults to sampleSeries if not provided.
- *
- * Usage:
- *   <Portfolio />
- *   <Portfolio series={mySeriesData} />
- */
 export default function Portfolio({ series = seriesList }) {
   const [activeSeries, setActiveSeries] = useState(null);
 
   return (
-    <div className="portfolio">
-      {activeSeries ? (
-        <SeriesPage
-          series={activeSeries}
-          onBack={() => setActiveSeries(null)}
-        />
-      ) : (
-        <SeriesList
-          series={series}
-          onSelectSeries={setActiveSeries}
-        />
-      )}
-    </div>
+    <>
+      <Menu></Menu>
+      <div className="portfolio">
+        {activeSeries ? (
+          <SeriesPage
+            series={activeSeries}
+            onBack={() => setActiveSeries(null)}
+          />
+        ) : (
+          <SeriesList
+            series={series}
+            onSelectSeries={setActiveSeries}
+          />
+        )}
+      </div>
+    </>
   );
 }

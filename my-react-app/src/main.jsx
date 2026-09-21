a37bb975254123d24ba29321c33bde6a65b66841
Rebecca <rebecca_517@hotmail.com>
2026-09-21 16:54:32 -0400

    create blank unified style sheet

--- my-react-app/src/main.jsx
+++ my-react-app/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App'
 import { BrowserRouter } from 'react-router-dom'
+import "./styles/typography.css";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
